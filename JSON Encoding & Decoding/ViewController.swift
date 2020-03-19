fe162d9f64a8b85e460b946171ae8b4c818c883e
umair-khan97 <umairkhan19997@gmail.com>
2020-03-19 16:16:08 +0500

    Initial Commit

---
+++ JSON Encoding & Decoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSON Encoding & Decoding
+//
+//  Created by Umair Khan on 19/03/2020.
+//  Copyright © 2020 Elexoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
